d2a0ecc9170a8d40e1f57f35331f7c2ad9bfc625
Keith Levine <keithlevine@Keiths-Air.attlocal.net>
2021-05-10 10:42:07 -0700

    random button picker working for computer turn

--- app.js
+++ app.js
@@ -8,12 +8,31 @@ const yellowButton = document.querySelector('.yellow-square');
 const redButton = document.querySelector('.red-square');
 const blueButton = document.querySelector('.blue-square');
 const greenButton = document.querySelector('.green-square');
+// const gameButtons = ['yellow-square', 'red-square', 'blue-square', 'green-square']
 
 // create computer turn array
 // create a human turn array
 
 let computerTurn = [];
 let playerTurn = [];
+let level = 0;
 
-// create a start game function to execute when button is clicked
+function randomButtonPicker() {
+    const gameButtons = ['yellow-square', 'red-square', 'blue-square', 'green-square'];
+    const randomColor = gameButtons[Math.floor(Math.random() * gameButtons.length)];
+    console.log(randomColor);
+}
 
+function nextLevel() {
+    level += 1;
+    const nextComputerTurn = [...computerTurn]
+    nextComputerTurn.push(randomButtonPicker)
+}
+
+function startGame() {
+    console.log('start')
+};
+
+startButton.addEventListener('click', startGame);
+
+randomButtonPicker()
