04000bd62dc99b87a7a6a1bbbfc9f6b5aa327628
Keith Levine <keithlevine@Keiths-Air.attlocal.net>
2021-05-10 09:09:17 -0700

    declared variables and created computer and player arrays

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('hi')
+// decalre my variables
+const startButton = document.querySelector('#button1');
+
+const computerTurnIndicator = document.querySelector('.computer');
+const playerTurnIndicator = document.querySelector('.player');
+
+const yellowButton = document.querySelector('.yellow-square');
+const redButton = document.querySelector('.red-square');
+const blueButton = document.querySelector('.blue-square');
+const greenButton = document.querySelector('.green-square');
+
+// create computer turn array
+// create a human turn array
+
+let computerTurn = [];
+let playerTurn = [];
+
+// create a start game function to execute when button is clicked
+
